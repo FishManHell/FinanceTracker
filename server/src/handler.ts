c8c20bd9da42fa83579648ec1c7498ba9c1fb02e
Denys <leonard66693@gmail.com>
2025-10-27 22:40:00 +0200

    another test for vercel

--- server/src/handler.ts
+++ server/src/handler.ts
@@ -1,31 +1,42 @@
+// import express from "express";
+// import { ApolloServer } from "@apollo/server";
+// import serverless from "serverless-http";
+// import cors from "cors";
+// import bodyParser from "body-parser";
+// import { schema } from "./graphql/schema.js";
+// import { context } from "./graphql/context.js";
+// import { connectDB } from "./mongo.js";
+// import { expressMiddleware } from '@as-integrations/express4'
+// import dotenv from "dotenv";
+//
+// dotenv.config();
+// console.log("✅ dotenv loaded");
+// await connectDB();
+// console.log("✅ MongoDB connected");
+//
+//
+// const app = express();
+// const server = new ApolloServer({ ...schema });
+//
+// await server.start();
+// console.log("✅ Apollo Server started");
+//
+// app.use(
+//   "/graphql",
+//   cors(),
+//   bodyParser.json(),
+//   expressMiddleware(server, { context })
+// );
+//
+// export const handler = serverless(app);
+
 import express from "express";
-import { ApolloServer } from "@apollo/server";
 import serverless from "serverless-http";
-import cors from "cors";
-import bodyParser from "body-parser";
-import { schema } from "./graphql/schema.js";
-import { context } from "./graphql/context.js";
-import { connectDB } from "./mongo.js";
-import { expressMiddleware } from '@as-integrations/express4'
-import dotenv from "dotenv";
-
-dotenv.config();
-console.log("✅ dotenv loaded");
-await connectDB();
-console.log("✅ MongoDB connected");
-
 
 const app = express();
-const server = new ApolloServer({ ...schema });
 
-await server.start();
-console.log("✅ Apollo Server started");
-
-app.use(
-  "/graphql",
-  cors(),
-  bodyParser.json(),
-  expressMiddleware(server, { context })
-);
+app.get("/graphql", (_req, res) => {
+  res.send("Hello from GraphQL!");
+});
 
 export const handler = serverless(app);
