a0d1d6756b168cc6f1a9efb070c8f0b767ce1c09
Denys <leonard66693@gmail.com>
2025-10-28 20:23:06 +0200

    test graphQl in server file

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { ApolloServer } from '@apollo/server';
 import cors from 'cors';
 import { json } from 'body-parser';
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@as-integrations/express4';
+import { expressMiddleware } from '@as-integrations/express4'
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
@@ -10,11 +13,18 @@ app.use(json());
 
 const server = new ApolloServer({
   typeDefs: `type Query { hello: String }`,
-  resolvers: { Query: { hello: () => 'world' } },
+  resolvers: { Query: { hello: () => 'world' } }
 });
 
-await server.start();
+async function startApollo() {
+  await server.start();
+  app.use('/graphql', expressMiddleware(server));
+}
 
-app.use('/graphql', expressMiddleware(server));
+startApollo();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from Express on Vercel!' });
+});
 
 export default app;
