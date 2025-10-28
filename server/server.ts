35d770fd3c7e0787c5e9a9067a38f956d0279b7f
Denys <leonard66693@gmail.com>
2025-10-28 17:21:10 +0200

    add apollo to server file for vercel

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { ApolloServer } from '@apollo/server';
 
 dotenv.config();
 
 const app = express();
 
+const server = new ApolloServer({ typeDefs: `type Query { hello: String }`, resolvers: { Query: { hello: () => 'world' } } });
+
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Express on Vercel!', env: process.env.NODE_ENV });
+  res.json({ message: 'Hello from Express on Vercel!' });
 });
 
 export default app;
