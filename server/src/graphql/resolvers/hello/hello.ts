9ac1d4300445cefe4b9c4746776197b1af386a07
Denys <leonard66693@gmail.com>
2025-10-29 11:38:03 +0200

    test my hello func

--- server/src/graphql/resolvers/hello/hello.ts
+++ server/src/graphql/resolvers/hello/hello.ts
@@ -1,12 +1,12 @@
 import { GraphQLErrorCode, HttpStatus, throwError } from '@/utils/errors.js'
 
+
 export const hello =  (_parent: any, _args: any, context: any) => {
   // console.log("👤 context.user:", context.user);
-  // if (!context.user) throwError({
-  //   message: "Unauthorized",
-  //   status: HttpStatus.UNAUTHORIZED,
-  //   code: GraphQLErrorCode.UNAUTHORIZED
-  // });
-  if (!context.user) return null;
+  if (!context.user) {
+    const err = new Error("Unauthorized");
+    (err as any).extensions = { code: "UNAUTHORIZED" };
+    throw err;
+  }
   return `Hello ${context.user.username}!`;
 };
