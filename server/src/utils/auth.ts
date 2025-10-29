a358aa8059267c679fba4b0b4e81081ec8278828
Denys <leonard66693@gmail.com>
2025-10-29 15:59:48 +0200

    test server function

--- server/src/utils/auth.ts
+++ server/src/utils/auth.ts
@@ -5,10 +5,13 @@ export const hashPassword = (password: string)  => {
   return bcrypt.hashSync(password, 10);
 }
 
-export const verifyPassword = (plain: string, hash: string) => bcrypt.compare(plain, hash);
+const verifyPassword = (plain: string, hash: string) => bcrypt.compare(plain, hash);
 
 export const generateToken = (payload: object)  => {
   const secret = process.env.JWT_SECRET;
   if (!secret) throw new Error('JWT_SECRET is not defined');
   return jwt.sign(payload, secret, { expiresIn: '5min' });
 }
+
+
+export default verifyPassword;
