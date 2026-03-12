c862a2187b1faf0d4f1e7e1ef66da1b1524bb3d9
Denys <leonard66693@gmail.com>
2026-03-12 15:34:20 +0200

    server: add changes to express start text

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  res.json({ message: '✅ Express Server is running' });
+  res.json({ message: '✅ Finance Tracker Server is running' });
 });
 
 app.use('/graphql', async (req, res, next) => {
