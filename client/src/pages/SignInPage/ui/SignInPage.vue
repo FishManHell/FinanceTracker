7f52e652df31968564233b7464db23301e859f7b
Denys <leonard66693@gmail.com>
2026-04-17 22:22:39 +0300

    client: dedup SignIn/SignUp SCSS via shared auth-card mixin

--- client/src/pages/SignInPage/ui/SignInPage.vue
+++ client/src/pages/SignInPage/ui/SignInPage.vue
@@ -21,15 +21,30 @@ const goToRegister = () => router.push({ name: AppRouters.SIGN_UP })
 </script>
 
 <template>
-  <div :class="cls.signin_page">
+  <div :class="cls.auth_page">
     <div :class="cls.card">
       <h1 :class="cls.title">Sign In</h1>
-      <input :class="cls.input" v-model="username" placeholder="Username" />
-      <input :class="cls.input" v-model="password" type="password" placeholder="Password" />
+      <input
+        id="signin-username"
+        name="username"
+        autocomplete="username"
+        :class="cls.input"
+        v-model="username"
+        placeholder="Username"
+      />
+      <input
+        id="signin-password"
+        name="password"
+        type="password"
+        autocomplete="current-password"
+        :class="cls.input"
+        v-model="password"
+        placeholder="Password"
+      />
       <button :class="cls.btn" @click="handleLogin" :disabled="isPending">Login</button>
       <p v-if="session_store.error" :class="cls.error">{{ session_store.error }}</p>
       <div :class="cls.divider" />
-      <button :class="classNames(cls.signup_btn, cls.btn)" @click="goToRegister">Sign Up</button>
+      <button :class="classNames(cls.secondary_btn, cls.btn)" @click="goToRegister">Sign Up</button>
     </div>
   </div>
 </template>
--- client/src/pages/SignUpPage/ui/SignUpPage.vue
+++ client/src/pages/SignUpPage/ui/SignUpPage.vue
@@ -28,16 +28,39 @@ const goToLogin = () => router.push({ name: AppRouters.SIGN_IN })
 </script>
 
 <template>
-  <div :class="cls.signup_page">
+  <div :class="cls.auth_page">
     <div :class="cls.card">
       <h2 :class="cls.title">Sign Up</h2>
-      <input :class="cls.input" v-model="username" placeholder="Username" />
-      <input :class="cls.input" v-model="email" placeholder="Email" />
-      <input :class="cls.input" v-model="password" type="password" placeholder="Password" />
+      <input
+        id="signup-username"
+        name="username"
+        autocomplete="username"
+        :class="cls.input"
+        v-model="username"
+        placeholder="Username"
+      />
+      <input
+        id="signup-email"
+        name="email"
+        type="email"
+        autocomplete="email"
+        :class="cls.input"
+        v-model="email"
+        placeholder="Email"
+      />
+      <input
+        id="signup-password"
+        name="password"
+        type="password"
+        autocomplete="new-password"
+        :class="cls.input"
+        v-model="password"
+        placeholder="Password"
+      />
       <button :class="cls.btn" @click="handleRegister" :disabled="isPending">Sign in</button>
       <p v-if="session_store.error" :class="cls.error">{{ session_store.error }}</p>
       <div :class="cls.divider" />
-      <button :class="classNames(cls.back_btn, cls.btn)" @click="goToLogin">
+      <button :class="classNames(cls.secondary_btn, cls.btn)" @click="goToLogin">
         Already have an account ? Sign In
       </button>
     </div>
